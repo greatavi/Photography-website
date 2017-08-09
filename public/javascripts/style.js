a8a6b7f01b1ebd3b44910dd13b7d2409b9604397
z <z@Macintosh.local>
2017-08-09 01:07:51 -0700

    First Commit

--- public/javascripts/style.js
+++ public/javascripts/style.js
@@ -1,17 +1,16 @@
 /**
  * Created by z on 6/12/17.
  */
-var index = 0;
-function carousel(){
-    var elements = document.getElementsByClassName("images");
-    console.log("elements:", elements);
-    for (var i = 0; i < elements.length; i++) {
-        elements[i].style.display = "none";
-    }
-    index++;
-    if(index>elements.length){
-        index = 1;
-    }
-    elements[index-1].style.display = "block";
-    setTimeout(carousel,2000);
-}
+$(document).ready(function(){
+    var carousel = $(".carousel").waterwheelCarousel({
+        flankingItems: 3
+    });
+    $('#prev').bind('click',function(){
+        carousel.prev();
+        return false;
+    });
+    $('#next').bind('click',function(){
+        carousel.next();
+        return false;
+    });
+});
